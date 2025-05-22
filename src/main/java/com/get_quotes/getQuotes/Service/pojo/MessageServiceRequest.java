98c57dd244062f18712324da57f820c31c509552
Himanshu Sehrawat2 <himanshusehrawat16032000@gmail.com>
2025-05-22 22:41:12 +0530

    changes

--- src/main/java/com/get_quotes/getQuotes/Service/pojo/MessageServiceRequest.java
+++ src/main/java/com/get_quotes/getQuotes/Service/pojo/MessageServiceRequest.java
@@ -22,9 +22,9 @@ public class MessageServiceRequest {
                 linkReceiverBot.sendText(Long.valueOf("773940189") , chatId + "price of "+getLinkDataResponse.getDescription()+" droped from \n" +
                         linkData.getPrice()+" to "+getLinkDataResponse.getPrice()+"\n" +
                         "check it out at "+ linkData.getUrl());
-            } else if (getLinkDataResponse.getPrice() > linkData.getPrice()) {
+            } else if (getLinkDataResponse.getPrice() > linkData.getPrice() && Math.random() < 0.35) {
                 linkReceiverBot.sendText(Long.valueOf("773940189") , chatId + "price of "+getLinkDataResponse.getDescription()+" increased from \n" +
-                        getLinkDataResponse.getPrice() + "to"+ linkData.getPrice()+"\n" +
+                        linkData.getPrice() + " to " + "\n" +
                         "check it out at "+ linkData.getUrl());
             }
         }
